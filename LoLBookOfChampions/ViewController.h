f19641e5d1ad4d19992b4d46dbdacf033c4b56bb
Jeff Roberts <jeff@nimbleNogginSoftware.com>
2015-01-19 19:19:46 -0600

    Initial Commit

---
+++ LoLBookOfChampions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoLBookOfChampions
+//
+//  Created by Jeff Roberts on 1/19/15.
+//  Copyright (c) 2015 nimbleNoggin.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoLBookOfChampions/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LoLBookOfChampions
+//
+//  Created by Jeff Roberts on 1/19/15.
+//  Copyright (c) 2015 nimbleNoggin.io. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
