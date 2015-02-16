cff75f7c7b51dd60f8f3e8e12213a8e0a3800ae3
Jeff Roberts <jeff@nimbleNogginSoftware.com>
2015-02-16 11:00:08 -0600

    Adding a cursor protocol for enhanced cursor operations

---
+++ LoLBookOfChampions/core/database/NIOCursor.h
@@ -0,0 +1,50 @@
+//
+// Created by Jeff Roberts on 2/16/15.
+// Copyright (c) 2015 io.nimblenoggin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NIOCursor <NSObject>
+@required 
+-(BOOL)boolForColumn:(NSString *)columnName;
+-(BOOL)boolForColumnIndex:(int)columnIndex;
+-(void)close;
+-(int)columnCount;
+-(int)columnIndexForName:(NSString *)columnName;
+-(BOOL)columnIndexIsNull:(int)columnIndex;
+-(BOOL)columnIsNull:(NSString *)columnName;
+-(NSString *)columnNameForIndex:(int)columnIndex;
+-(NSData *)dataForColumn:(NSString *)columnName;
+-(NSData *)dataForColumnIndex:(int)columnIndex;
+-(NSDate *)dateForColumn:(NSString *)columnName;
+-(NSDate *)dateForColumnIndex:(int)columnIndex;
+-(double)doubleForColumn:(NSString *)columnName;
+-(double)doubleForColumnIndex:(int)columnIndex;
+-(int)intForColumn:(NSString *)columnName;
+-(long)longForColumn:(NSString *)columnName;
+-(long)longForColumnIndex:(int)columnIndex;
+-(long long int)longLongIntForColumn:(NSString *)columnName;
+-(long long int)longLongIntForColumnIndex:(int)columnIndex;
+-(BOOL)move:(int)offset;
+-(BOOL)moveToFirst;
+-(BOOL)moveToLast;
+-(BOOL)moveToPostion:(uint)position;
+-(BOOL)next;
+-(BOOL)previous;
+-(int)rowCount;
+-(NSString *)stringForColumn:(NSString *)columnName;
+-(NSString *)stringForColumnIndex:(int)columnIndex;
+-(unsigned long long int)unsignedLongLongIntForColumn:(NSString *)columnName;
+-(unsigned long long int)unsignedLongLongIntForColumnIndex:(int)columnIndex;
+
+@optional
+-(NSData *)dataNoCopyForColumn:(NSString *)columnName NS_RETURNS_NOT_RETAINED;
+-(NSData *)dataNoCopyForColumnIndex:(int)columnIndex NS_RETURNS_NOT_RETAINED;
+-(id)objectForColumnName:(NSString *)columnName;
+-(id)objectForColumnIndex:(int)columnIndex;
+-(id)objectForKeyedSubscript:(NSString *)columnName;
+-(id)objectAtIndexedSubscript:(int)columnIndex;
+-(const unsigned char *)UTF8StringForColumnName:(NSString *)columnName;
+-(const unsigned char *)UTF8StringForColumnIndex:(int)columnIndex;
+@end
