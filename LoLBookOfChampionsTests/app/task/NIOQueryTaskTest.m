9859a9731d1a554d9525f8766cd1cf292de33040
Jeff Roberts <jeff@nimbleNogginSoftware.com>
2015-02-24 05:26:32 -0600

    Configuration change supporting Kiwi unit tests and first tests

---
+++ LoLBookOfChampionsTests/app/task/NIOQueryTaskTest.m
@@ -0,0 +1,72 @@
+//
+// NIOQueryTaskTest / LoLBookOfChampions
+//
+// Created by Jeff Roberts on 2/24/15.
+// Copyright (c) 2015 Riot Games. All rights reserved.
+//
+
+
+#import <Kiwi/Kiwi.h>
+#import "NIOContentResolver.h"
+#import "NIOQueryTask.h"
+#import "NIODataDragonContract.h"
+
+SPEC_BEGIN(NIOQueryTaskSpec)
+	describe(@"With a query task", ^{
+
+		__block NIOContentResolver *mockContentResolver;
+		__block NIOQueryTask *queryTask;
+		__block BFTask *promise;
+
+		context(@"when the query task is run", ^{
+
+			beforeEach(^{
+				mockContentResolver = [NIOContentResolver mock];
+				queryTask = [[NIOQueryTask alloc] initWithContentResolver:mockContentResolver];
+				promise = [BFTask taskWithResult:nil];
+			});
+
+			it(@"it should invoke the query through the content resolver", ^{
+				[[mockContentResolver should]
+						receive:@selector(queryWithURI:withProjection:withSelection:withSelectionArgs:withGroupBy:withHaving:withSort:)
+					  andReturn:promise
+				  withArguments:any(), any(), any(), any(), any(), any(), any()];
+				[queryTask runAsync];
+			});
+
+			it(@"it should invoke the query passing the projection, selection, selectionArgs, group by, having and sort", ^{
+				NSURL *uri = [Champion URI];
+				NSArray *projection = @[[ChampionColumns COL_NAME], [ChampionColumns COL_TITLE]];
+				NSString *selection = [NSString stringWithFormat:@"%@ = ?", [ChampionColumns COL_NAME]];
+				NSArray *selectionArgs = @[@"Heimerdinger"];
+				NSString *groupBy = nil;
+				NSString *having = nil;
+				NSString *sort = [NSString stringWithFormat:@"%@", [ChampionColumns COL_NAME]];
+
+				queryTask.uri = uri;
+				queryTask.projection = projection;
+				queryTask.selection = selection;
+				queryTask.selectionArgs = selectionArgs;
+				queryTask.groupBy = groupBy;
+				queryTask.having = having;
+				queryTask.sort = sort;
+
+				[[mockContentResolver should]
+						receive:@selector(queryWithURI:withProjection:withSelection:withSelectionArgs:withGroupBy:withHaving:withSort:)
+					  andReturn:promise
+				  withArguments:uri, projection, selection, selectionArgs, groupBy, having, sort];
+				[queryTask runAsync];
+			});
+
+			it(@"it should return a promise when invoked", ^{
+				[[mockContentResolver should]
+						receive:@selector(queryWithURI:withProjection:withSelection:withSelectionArgs:withGroupBy:withHaving:withSort:)
+					  andReturn:promise
+				  withArguments:any(), any(), any(), any(), any(), any(), any()];
+				BFTask *promiseResult = [queryTask runAsync];
+				[[promiseResult shouldNot] beNil];
+			});
+		});
+	});
+
+SPEC_END
--- Podfile
+++ Podfile
@@ -1,17 +1,15 @@
 # Uncomment this line to define a global platform for your project
 platform :ios, '8.1'
 
-pod 'AFNetworking', '2.5.1'
-pod 'Bolts', '1.1.3'
-pod 'CocoaLumberjack', '2.0.0-rc'
-pod 'FMDB', '2.5'
-pod 'Typhoon', '3.0.1'
-
-target 'LoLBookOfChampions' do
-
+target :LoLBookOfChampions, :exclusive => true do
+  pod 'AFNetworking', '2.5.1'
+  pod 'Bolts', '1.1.3'
+  pod 'CocoaLumberjack', '2.0.0-rc2'
+  pod 'FMDB', '2.5'
+  pod 'Typhoon', '3.0.1'
 end
 
-target 'LoLBookOfChampionsTests' do
-
+target :LoLBookOfChampionsTests, :exclusive => true do
+  pod 'Kiwi', '2.3.1'
 end
 
